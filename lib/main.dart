b77dd8076d4f9cd393a319ea8b8be09b7cd73157
Vamshi Krishna <vamsikrishna2644@gmail.com>
2024-06-30 16:15:30 +0100

    improvements in app perfomance

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await dotenv.load(fileName: "keys.env");
+  await dotenv.load(fileName: ".env");
   GoogleFonts();
   runApp(const ProviderScope(child: MyApp()));
 }
